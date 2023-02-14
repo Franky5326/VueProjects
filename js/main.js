2359b252e7be3195066df49a8b7e84bcc593bd2e
Дмитрий Никитин <gr404_nidal@lab-402-2.tomtit-tomsk.ru>
2023-02-14 11:37:40 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks.",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "More products like this.",
+        inStock: true,
+        inventory: 100
+    }
+ })
+ 
