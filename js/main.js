b1bcb2473870eb56baacbc913fd7cfa60cc4b6b2
Dmitriy Nikitin <franky5326@yandex.ru>
2023-02-14 20:58:03 +0700

    6 практическая

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "More products like this.",
         inStock: true,
-        inventory: 100,
+        inventory: 0,
         OnSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         sizes: ['S','M','L','XL','XXL','XXXL'],
